8f4adc0540d4e1877fe44c354656f68c6a7f2f74
Thavas Antonio <airstalk3r@gmail.com>
2023-06-17 15:10:34 -0700

    register/login dont appear when logged in now (homepage)

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -8,10 +8,29 @@
         <h2 class="home-subtitle">Step into Serenity: Discover, Rate, and Share Inspiring Walking Routes</h2>
       </div>
     <div class="d-flex flex-column home-auth">
-        <router-link to="/register" class="btn btn-primary button mb-3 ">Register</router-link>
-        <router-link to="/login" class="btn btn-primary button">Login</router-link>
+        <router-link  v-if="loggedOut" to="/register" class="btn btn-secondary button mb-3 ">Register</router-link>
+        <router-link  v-if="loggedOut" to="/login" class="btn btn-secondary button">Login</router-link>
     </div>
 
     </div>
   </template>
   
+  <script>
+  export default {
+    data() {
+      return {
+        loggedOut: true
+      };
+    },
+    mounted() {
+      // Check if 'accessToken' exists in localStorage
+      const accessToken = localStorage.getItem('accessToken');
+      if (accessToken) {
+        this.loggedOut = false;
+      }
+      else {
+        this.loggedOut = true;
+      }
+    }
+  };
+  </script>
