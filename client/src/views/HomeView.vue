67e04fbcb478346c05b0600f73dc1eac4309fa20
Thavas Antonio <airstalk3r@gmail.com>
2023-06-21 20:58:55 -0700

    home page layout re-design along with minor scss changes to other pages

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,19 +1,70 @@
 <template>
     <div class="home-container">
-      <div class="home-truck">
-        <img src="../../public/images/truck.png" alt="truck" />
-      </div>
-      <div class="d-flex flex-column align-items-start home-ts">
+      <div class="first-container d-flex justify-content-center">
         <h1 class="home-title">Yum Union</h1>
         <h2 class="home-subtitle">Wasting None, Feeding Everyone!"</h2>
       </div>
-    <div class="d-flex flex-column home-auth">
-        <router-link  v-if="loggedOut" to="/register" class="btn btn-secondary button mb-3 ">Register</router-link>
-        <router-link  v-if="loggedOut" to="/login" class="btn btn-secondary button">Login</router-link>
-    </div>
+      <div class="second-container container">
+          <div class="row ">
+            <div class="col">
+              <h3 class="home-info-title">Restaurants & Food Banks United</h3>
+            </div>
+            <div class="col">
+              <p class="home-info-text">Yum Union bridges the gap between restaurants with surplus food and food banks seeking donations. Join this revolutionary platform to reduce waste and hunger together!</p>
+            </div>
+          </div>
+      </div>
+      <div class="third-container container">
+        <div class="row mb-4">
+          <div class="col-md-9 ">
+            <h3 class="home-info-title">Ready to make a difference? Join Food Connect.</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-2">
+            <div class="home-auth row d-flex justify-content-between mb-5">
+              <router-link v-if="loggedOut" to="/register" class="col btn btn-secondary button me-1">Register</router-link>
+              <router-link v-if="loggedOut" to="/login" class="col btn btn-secondary button ms-1" style="background-color: #5465FF;">Login</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="fourth-container container">
+        <div class="row row-cols-2 align-items-center">
+          <div class="col-7">
+            <h3 class="home-info-title-sm mb-5">Intuitive Dashboard for Food Surplus Management </h3>
+            <p class="home-info-text">As a <strong>restaurant</strong>, easily manage surplus food ensuring your restaurant’s excess is put to use for the betterment of the local community. As a <strong>food bank</strong> browse and request food donations. </p>
+          </div>
+          <div class="col-5">
+            <img src="../../public/images/home-dashboard.png" alt="Image" class="home-img">
+          </div>
+        </div>
+      </div>
 
+        <div class="fifth-container container">
+          <div class="row">
+            <div class="col-12 text-left mb-5">
+              <h3 class="home-info-title">Frequently Asked Questions</h3>
+            </div>
+          </div>
+          <div class="row">
+              <div class="col-md-4">
+                  <div class="row-6 home-info-text"><strong>How Can I Join</strong></div>
+                  <div class="row-6 home-info-text"><p>Simply click the “Sign Up” button and provide the details of your restaurant or food bank.</p></div>
+              </div>
+              <div class="col-md-4">
+                  <div class="row-6 home-info-text"><strong>Is it free?</strong></div>
+                  <div class="row-6 home-info-text"><p>Absolutely! Food Connect is free for both restaurants and food banks.</p></div>
+              </div>
+              <div class="col-md-4">
+                  <div class="row-6 home-info-text"><strong>How Can I Contribute</strong></div>
+                  <div class="row-6 home-info-text"><p>We are an open source project. You can help out through our <a href="https://github.com/youngwonks-coding-school/ywsos2023" target="_blank"><i class="fab fa-github"></i> GitHub</a> </p></div>
+              </div>
+            </div>
+        </div>
     </div>
   </template>
+
   
   <script>
   export default {
--- client/src/views/ProfileView.vue
+++ client/src/views/ProfileView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mt-5 mb-5">
+  <div class="profile-container container">
     <div class="title-container d-flex justify-content-center flex-nowrap">
       <h1 class="p-title">Let's Figure Out Who You Are!</h1>
     </div>
@@ -14,38 +14,40 @@
     <div class="form-container d-flex justify-content-center flex-nowrap" v-on:submit.prevent="onSubmit">
 
       <form class="row form">
-        <div class="col-md-6 form-group">
-          <label for="name">Name:</label>
+        <div class="row-md-6 form-group">
+          <label for="name">Business Name:</label>
           <input type="text" id="name" class="form-control" v-model="name">
         </div>
 
-        <div class="col-md-6 form-group">
-          <label for="address">Address:</label>
+        <div class="row-md-6 form-group">
+          <label for="address">Business Address:</label>
           <input type="text" id="address" class="form-control" v-model="address">
         </div>
         <div class="w-100"></div>
-        <div class="col-md-6 form-group">
-          <label for="number">Phone Number:</label>
-          <input type="text" id="phone" class="form-control" v-model="phone">
-        </div>
 
-        <div class="col-md-6 form-group">
+        <div class="row-md-6 form-group">
           <label for="city">City:</label>
           <input type="text" id="city" class="form-control" v-model="city">
         </div>
         <div class="w-100"></div>
-        <div class="col-md-6 form-group">
-          <label for="state">State:</label>
-          <input type="text" id="state" class="form-control" v-model="state">
-        </div>
+        <div class="row form-group">
+          <div class="col-md-6 form-group">
+            <label for="state">State:</label>
+            <input type="text" id="state" class="form-control" v-model="state">
+          </div>
 
-        <div class="col-md-6 form-group">
-          <label for="country">Country:</label>
-          <input type="text" id="country" class="form-control" v-model="country">
+          <div class="col-md-6 form-group">
+            <label for="country">Country:</label>
+            <input type="text" id="country" class="form-control" v-model="country">
+          </div>
+        </div>
+        <div class="row-md-6 form-group">
+            <label for="number">Phone Number:</label>
+            <input type="text" id="phone" class="form-control" v-model="phone">
         </div>
         <div class="col-md-12 d-flex justify-content-center">
           <div class="submit-button">
-            <input type="submit" @click="onSubmit()" class="btn btn-primary button" value="Submit"/>
+            <input type="submit" @click="onSubmit()" class="btn btn-primary form-button" value="Submit"/>
           </div>
         </div>
       </form>
@@ -92,7 +94,7 @@ export default {
           }
           axios.post('/api/auth/profile_data', data).then((response) => {
             const responseData = JSON.parse(JSON.stringify(response.data.message))
-            console.log('buisness count' + responseData[0])
+            console.log('buisness count' + responseData)
             this.yelp_response = responseData
           }).catch((error) => {
             console.log(error)
